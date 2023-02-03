15661ecebe29ef352f228e68c5d84fc09b4763fd
Ankur Kumar <106395652+ankur766@users.noreply.github.com>
2023-02-04 00:09:07 +0530

    Time: 10 ms (62.86%), Space: 14 MB (92.06%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int x=0;
+        
+        int x=0; // declare x=0;
+        
         for(int i=0;i<operations.size();i++)
         {
-            if(operations[i]=="--X" || operations[i]=="X--")
+            if(operations[i]=="--X" || operations[i]=="X--") // check contion  if operation of index i == --x or x-- then x will mins 1 
             {
                 x -=1;
             }
-             else if ( operations[i]=="X++" || operations[i]=="++X" )
+             else if ( operations[i]=="X++" || operations[i]=="++X" )// check contion  if operation of index i == --x or x-- then x will plus 1 
+            
              {
                  x+=1;
              }
             else{
-                continue;
+                continue; //  if both contion will not satifed then  it  loop will cotinue to check next index 
             }
         }
-        return x;
+        
+        return x; // return answer 
         
     }
 };
