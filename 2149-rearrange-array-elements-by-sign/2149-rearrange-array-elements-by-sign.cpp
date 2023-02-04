7e5476e6ee9d224846b87e1f8fb76e29868f1784
Ankur Kumar <106395652+ankur766@users.noreply.github.com>
2023-02-04 23:28:35 +0530

    Time: 228 ms (84.72%), Space: 124 MB (69.21%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>ans_store(nums.size());
+        
+        vector<int>ans_store(nums.size()); // decalre vector  eqal to size of nums
         
             int even=0;
         
         int odd= 1;
         
-        for(int i=0;i<nums.size();i++)
-        { if (nums[i]>0)
+        for(int i=0;i<nums.size();i++) 
+        { if (nums[i]>0)                // check nums[i]is npostive then store in even inex
         {
             ans_store[even]=nums[i];
             even+=2;
         }
          else{
-              ans_store[odd]=(nums[i]);
+              ans_store[odd]=(nums[i]);// if not is even  then it is store in odd index  number of  ans_store vector 
              
-            odd+=2;
+            odd+=2; // odd will  increment by 2 
 }
 }
-        return ans_store;
+        return ans_store;  // lst return answer
         
         
         
