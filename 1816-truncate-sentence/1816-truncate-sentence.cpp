356820029d1c49c3fcb55c696611a80fe03d2e8b
Ankur Kumar <106395652+ankur766@users.noreply.github.com>
2023-02-06 23:43:00 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (85.86%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     string truncateSentence(string s, int k) 
     {
-        string ans = "", temp = "";
-        int i = 0, j = 0;
-        while(i < k && j <= s.size())
+        int space = 0 ; 
+        int i = 0 ; 
+        int n = s.size() ; 
+        string result_ ="";
+        while ((i != n))
         {
-            if(s[j] != ' ')
-                temp += s[j];
-            else
+            if (s[i] == ' ')
             {
-                ans += temp;
-                temp = "";
-                if(i < k - 1)
-                    ans += ' ';
-                i++;
+                space ++;
             }
-            if(j == s.size() - 1)
-                ans += temp;
-            j++;
-        }    
-        return ans;
+            if(space == k)
+            {
+                break ;
+            }
+            result_ += s[i];   
+            i++; 
+        }
+        return result_;
     }
 };
