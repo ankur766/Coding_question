541fd781c2d6a154668c5d0395a2952b6dd633b2
Ankur Kumar <106395652+ankur766@users.noreply.github.com>
2023-02-14 01:45:32 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (19.45%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -4,9 +4,12 @@ public:
         string parser;
         
        for(int i=0;i<command.length();i++){
-            if(command[i]=='G')  parser+=command[i];
-            if(command[i]=='(' && command[i+1]==')')  parser+='o';
-            if(command[i]=='(' && command[i+1]=='a'&&command[i+2]=='l' )  parser+="al"; 
+            if(command[i]=='G') 
+                parser+=command[i];
+            if(command[i]=='(' && command[i+1]==')')  
+                parser+='o';
+            if(command[i]=='(' && command[i+1]=='a'&&command[i+2]=='l' ) 
+                parser+="al"; 
         }
         return  parser;
         
