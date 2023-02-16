0d6c749307db67aff5daaeb165aa5dee5e39959e
Ankur Kumar <106395652+ankur766@users.noreply.github.com>
2023-02-17 01:24:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.42%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    void  solve(vector<int>v,int k, int index,int &ans)
-    {int n=v.size();
-        if(v.size()==1)
-        {
-            ans=v[0];
-            return ;
-        }
-        index=(index+k)% v.size();
-        v.erase(v.begin()+index);
-        solve(v,k,index,ans);
+//     void  solve(vector<int>v,int k, int index,int &ans)
+//     {int n=v.size();
+//         if(v.size()==1)
+//         {
+//             ans=v[0];
+//             return ;
+//         }
+//         index=(index+k)% v.size();
+//         v.erase(v.begin()+index);
+//         solve(v,k,index,ans);
        
-    }
+//     }
     
-    int findTheWinner(int n, int k) {
-        vector<int>v;
-        for(int i=1;i<=n;i++)
-        {v.push_back(i);
+//     int findTheWinner(int n, int k) {
+//         vector<int>v;
+//         for(int i=1;i<=n;i++)
+//         {v.push_back(i);
             
-        }
-        k--;
-        int index=0;
-        int ans=0;
-         solve(v,k,index,ans);
+//         }
+//         k--;
+//         int index=0;
+//         int ans=0;
+//          solve(v,k,index,ans);
         
-        return ans;}
+//         return ans;}
+    int solve(int n,int k)
+    {
+        if(n==1)
+        {
+            return 0;
+        }
+        return (solve(n-1,k)+k)%n;
+    }
+    int findTheWinner(int n, int k) {
+        int ans=solve(n,k)+1;
+         return  ans;
+    }
     
 };
