3a6e1c4fc6d88a2dfdfe2bf403071cfaab629c25
Ankur Kumar <106395652+ankur766@users.noreply.github.com>
2023-02-19 19:58:34 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (11.49%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int size=max(s.size(),t.size());
-        for(int i=0;i<size;i++)
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // int size=max(s.size(),t.size());
+        // for(int i=0;i<size;i++)
+        // {
+        //     if(s[i]!=t[i])
+        //     {
+        //         return t[i];
+        //     }
+        // }
+        // return NULL;
+      unordered_map<char, int>h;
+        for(int i=0;i<s.size();i++)
         {
-            if(s[i]!=t[i])
-            {
-                return t[i];
-            }
+            h[s[i]]++;
+            
         }
+        for(int i=0;i<t.size();i++)
+        {
+          if(--h[t[i]]<0)
+          {
+              return t[i];
+          }
+           
+        }
+             
         return NULL;
+
         
     }
 };
